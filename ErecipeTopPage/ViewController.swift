b1f10d9eef9784a1e9c75292a1ef68615f96aa2e
yuichiro.tsuji <yuichiro.tsuji@excite.jp>
2019-09-30 10:22:03 +0900

    Initial Commit

---
+++ ErecipeTopPage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ErecipeTopPage
+//
+//  Created by 辻 悠一郎 on 2019/09/30.
+//  Copyright © 2019 辻 悠一郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ErecipeTopPageTests/ErecipeTopPageTests.swift
@@ -0,0 +1,34 @@
+//
+//  ErecipeTopPageTests.swift
+//  ErecipeTopPageTests
+//
+//  Created by 辻 悠一郎 on 2019/09/30.
+//  Copyright © 2019 辻 悠一郎. All rights reserved.
+//
+
+import XCTest
+@testable import ErecipeTopPage
+
+class ErecipeTopPageTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
